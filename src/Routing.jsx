f168cc785540600f481e4534f5979403db4404f6
debanshu <debanshu78@gmail.com>
2025-05-05 03:20:29 +0530

    feat: add favicon and web app manifest; create new components for About, Contact, Skills, Testimonials, and TestimonialsForm

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import App from "./App";
+import TestimonialsForm from "./pages/TestimonialForm/TestimonialsForm";
 
 const Routing = () => {
   console.log("hii");
   return (
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/testimonials-post" element={<TestimonialsForm />} exact />
     </Routes>
   );
 };
---
+++ src/pages/TestimonialForm/TestimonialsForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestimonialsForm = () => {
+  return <div>TestimonialsForm</div>;
+};
+
+export default TestimonialsForm;
